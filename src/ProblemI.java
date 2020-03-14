2f7722bc9b6ffaa3d49b3a92f155eb1b281e74c3
Garth Slaney <45348251+GarthS1@users.noreply.github.com>
2020-03-14 14:55:01 -0600

    Update ProblemI.java

--- src/ProblemI.java
+++ src/ProblemI.java
@@ -1,4 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ProblemI {
-
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int currentTime = scan.nextInt();
+		int classTime = scan.nextInt();
+		int numTranistRoutes = scan.nextInt();
+		ArrayList<Integer> walkTimes = new ArrayList<Integer>();
+		ArrayList<Integer> busTimes = new ArrayList<Integer>();
+		ArrayList<Integer> arrivalTimes = new ArrayList<Integer>();
+		
+		for( int i = 0; i <= numTranistRoutes ; i++ ) {
+			 walkTimes.add(scan.nextInt());
+		}
+		
+		for( int i = 0; i < numTranistRoutes ; i++ ) {
+			busTimes.add(scan.nextInt());
+		}
+		
+		for( int i = 0; i < numTranistRoutes ; i++ ) {
+			arrivalTimes.add(scan.nextInt());
+		}
+		
+		int index = 0;
+		while(index < numTranistRoutes ) {
+			currentTime += walkTimes.get(index);
+			for(; ( currentTime % arrivalTimes.get(index) ) != 0; currentTime++) {}
+			currentTime += busTimes.get(index);
+			index++;
+		}
+		
+		currentTime += walkTimes.get(index);
+		
+		if( classTime >= currentTime ) {
+			System.out.println("yes");
+		}
+		else {
+			System.out.println("no");
+		}
+	}
 }
