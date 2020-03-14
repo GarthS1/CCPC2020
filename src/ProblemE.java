559e4daa698c4d8d40182f3ad632200a4f84a069
EddieSKim <59934288+EddieSKim@users.noreply.github.com>
2020-03-14 15:09:15 -0600

    Update ProblemE.java

--- src/ProblemE.java
+++ src/ProblemE.java
@@ -1,7 +1,106 @@
 
+import java.util.Scanner;
+
 public class ProblemE {
+
+	static public int[] convertNotes(int length, String[] src) {
+		int[] convert = new int[length];
+		
+		for(int i = 0; i < length; i++) {
+			if(src[i].compareTo("C") == 0)
+				convert[i] = 1;
+			else if(src[i].compareTo("C#") == 0)
+				convert[i] = 2;
+			else if(src[i].compareTo("D") == 0)
+				convert[i] = 3;
+			else if(src[i].compareTo("D#") == 0)
+				convert[i] = 4;
+			else if(src[i].compareTo("E") == 0)
+				convert[i] = 5;
+			else if(src[i].compareTo("F") == 0)
+				convert[i] = 6;
+			else if(src[i].compareTo("F#") == 0)
+				convert[i] = 7;
+			else if(src[i].compareTo("G") == 0)
+				convert[i] = 8;
+			else if(src[i].compareTo("G#") == 0)
+				convert[i] = 9;
+			else if(src[i].compareTo("A") == 0)
+				convert[i] = 10;
+			else if(src[i].compareTo("A#") == 0)
+				convert[i] = 11;
+			else if(src[i].compareTo("B") == 0)
+				convert[i] = 12;
+		}
+		return convert;
+	}
+	
+	static public boolean checkTransposition(int length, int[] m1, int[] m2) {
+		int range = m2[0] - m1[0];
+		
+		for(int i = 1; i < length; i++) {
+			int noteRange = m2[i] - m1[i];
+			if(noteRange != range)
+				return false;
+		}
+		return true;
+	}
+	
+	static public boolean checkInversion(int length, int[] m1, int[] m2) {
+		
+		for(int i = 0; i < length-1; i++) {
+			int difference = m1[i+1] - m1[0];
+			int difference2 = m2[0] - m2[i+1];
+			if(difference2 < 0 )
+				difference2 += 12;
+			
+			if(difference2 != difference)
+				return false;
+		}
+		return true;
+	}
+	
+	static public boolean checkRetrograde(int length, int[]m1, int[]m2) {
+		for(int i = 0; i < length; i++) {
+			if(m1[i] != m2[length -1-i])
+				return false;
+		}
+		
+		return true;
+	}
 	
 	public static void main(String[] args) {
 		
+		Scanner in = new Scanner(System.in);
+		int melody = in.nextInt();
+		
+		String[] firstMelody = new String[melody];
+		String[] secondMelody = new String[melody];
+		
+		Scanner scan = new Scanner(System.in);
+		for(int i = 0; i < melody; i++)
+		{
+		    firstMelody[i] = scan.next();
+		}
+		Scanner scan1 = new Scanner(System.in);
+		for(int i = 0; i < melody; i++)
+		{
+		    secondMelody[i] = scan1.next();
+		}
+
+		int[] melodyOne = convertNotes(melody,firstMelody);
+		int[] melodyTwo = convertNotes(melody,secondMelody);
+		
+		
+		if(checkTransposition(melody,melodyOne, melodyTwo))
+			System.out.println("Transposition");
+		else if(checkInversion(melody,melodyOne, melodyTwo))
+			System.out.println("Inversion");
+		else if(checkRetrograde(melody,melodyOne,melodyTwo))
+			System.out.println("Retrograde");
+		else
+			System.out.println("Nonsense");
+		
 	}
 }
+
